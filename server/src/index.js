c43b19bf1785ba865016bd4dd3b048d011275559
saminchandeepa <saminchandeepa@gmail.com>
2023-06-05 21:25:01 +0530

    setup env file

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,11 @@ const axios = require("axios");
 
 const app = express();
 
+// Config
+require("dotenv").config();
+
+console.log(process.env.APP_ID);
+
 // Middleware
 app.use(express.json());
 app.use(cors());
@@ -13,7 +18,7 @@ app.get("/convert", async (req, res) => {
   const { date, sourceCurrency, targetCurrency, amountInSourceCurrency } =
     req.query;
 
-  const currencyURL = `https://openexchangerates.org/api/historical/${date}.json?${process.env.APP_ID}`;
+  const currencyURL = `https://openexchangerates.org/api/historical/${date}.json?app_id=${process.env.APP_ID}`;
   const namesURl = `https://openexchangerates.org/api/currencies.json?app_id=${process.env.APP_ID}`;
   try {
     const response = await axios.get(currencyURL);
